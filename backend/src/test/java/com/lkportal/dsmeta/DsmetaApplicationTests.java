987e01a3a6b9f7983aca4a647c98264597b08b8c
lkportal <lksxim@gmail.com>
2022-07-15 13:44:02 -0300

    projeto criado

---
+++ backend/src/test/java/com/lkportal/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lkportal.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
