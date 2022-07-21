1231a0b0d0758891c330eb12f603edc00f89b71a
lkportal <lksxim@gmail.com>
2022-07-21 18:49:39 -0300

    data select

--- backend/src/main/java/com/lkportal/dsmeta/resources/SalesResource.java
+++ backend/src/main/java/com/lkportal/dsmeta/resources/SalesResource.java
@@ -1,10 +1,13 @@
 package com.lkportal.dsmeta.resources;
 
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lkportal.dsmeta.entities.Sales;
@@ -18,8 +21,10 @@ public class SalesResource {
 	SalesService services;
 	
 	@GetMapping
-	public List<Sales> findAll(){
-		return services.findSales();
+	public Page<Sales> findAll(@RequestParam(value ="minDate",defaultValue = "") String minDate,
+			@RequestParam(value="maxDate",defaultValue = "") String maxDate,
+			Pageable pageable ){
+		return services.findSales(minDate,maxDate,pageable);
 	}
 	
 	
